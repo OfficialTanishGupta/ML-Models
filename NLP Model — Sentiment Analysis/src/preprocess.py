bbfb581ab3a34452c7fafc91e3f2242cd6306c36
Tanish Gupta <Officialtanishgupta@gmail.com>
2026-03-05 12:51:49 +0530

    Advance implementation of TF-IDF based sentiment analysis using Logistic Regression

--- NLP Model — Sentiment Analysis/src/preprocess.py
+++ NLP Model — Sentiment Analysis/src/preprocess.py
@@ -1,6 +1,17 @@
-import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
 
-def load_data(path):
-    df = pd.read_csv(path)
-    df['sentiment'] = df['sentiment'].map({'positive':1,'negative':0})
-    return df
+stop_words = set(stopwords.words("english"))
+
+def clean_text(text):
+
+    text = text.lower()
+
+    text = re.sub(r"[^a-zA-Z]", " ", text)
+
+    words = text.split()
+
+    words = [w for w in words if w not in stop_words]
+
+    return " ".join(words)
